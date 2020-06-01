2d881338d70d18f70731e4c9e003f38157021624
Kalana <Kalanam217@gmail.com>
2020-06-01 22:10:22 +0530

    Train details interfaces

--- lib/User/Searchtrain.dart
+++ lib/User/Searchtrain.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Searchtrain extends StatefulWidget {
   @override
   _SearchtrainState createState() => _SearchtrainState();
@@ -11,26 +10,59 @@ class _SearchtrainState extends State<Searchtrain> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
-      body: Center(child: Trainview(),),
+      body: Center(
+        child: Trainview(),
+      ),
     );
   }
 }
 
-
-class Trainview extends StatelessWidget{
+class Trainview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 100,
-          decoration: BoxDecoration(
-            
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16))),
           ),
-        )
-      ],
+         Container(
+           color:Colors.white,
+           child:   Row(
+            children: <Widget>[
+              SizedBox(height: 20,
+              width: 10,
+              child: DecoratedBox(decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16)
+              )),),
+              Expanded(
+                              child: LayoutBuilder(builder:(context,constraints){
+                  return Flex(children: List.generate((constraints.constrainWidth()/10).floor(), (index)=>SizedBox(height: 1,width:5 ,child: DecoratedBox(decoration: BoxDecoration(color: Colors.grey.shade800),),),
+                  ),
+                  direction:Axis.horizontal,
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,);
+                },),
+              ),
+            ],
+          ),
+         ),
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16))),
+          )
+        ],
+      ),
     );
   }
-  
 }
