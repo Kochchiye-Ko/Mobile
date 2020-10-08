ee5a5d505375b21a93e48022dbd7339ffa66b8dd
Kalana <Kalanam217@gmail.com>
2020-10-08 11:46:59 +0530

    Lost and Found basic

--- lib/User/FoundandLost.dart
+++ lib/User/FoundandLost.dart
@@ -8,6 +8,28 @@ class Foundandlost extends StatefulWidget {
 class _FoundandlostState extends State<Foundandlost> {
   @override
   Widget build(BuildContext context) {
-    return Scaf;
+    MediaQueryData data = MediaQuery.of(context);
+    double defaultSize = data.orientation == Orientation.landscape
+        ? data.size.height * 0.24
+        : data.size.width * 0.24;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lost and Found"),
+      ),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 10,
+          ),
+          AspectRatio(
+              aspectRatio: 1.55,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(defaultSize * 1.0),
+                    color: Color(0xFFD82D40)),
+              )),
+        ],
+      ),
+    );
   }
 }
